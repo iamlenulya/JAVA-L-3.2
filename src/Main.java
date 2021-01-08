d3e4b385f0d424e54278a7a58b407dac2268651e
Elena Degteva <lavishblanket@gmail.com>
2021-01-08 16:43:41 +0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         long balance = 100;
-        long refill = 1600;
-        long amount;
+        long refill = 1100;
         long bonus = refill / 100;
         if (refill <= 100) {
             bonus = 0;
-            amount = balance + refill;
+        }
+        else {
+            balance = balance + refill;
         }
         if (refill > 100) {
             bonus = refill / 100;
-            balance = refill + bonus;
         }
-        System.out.println(bonus);
+        else {
+            balance = balance + refill + bonus;
     }
+        System.out.println(bonus);
+        System.out.println(balance);
+        }
 }
