a9f5a922a9a66a3404a36cb993b31abff4775826
Elena Degteva <lavishblanket@gmail.com>
2021-01-12 21:13:01 +0400

    newhope

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,15 @@ public class Main {
     public static void main(String[] args) {
         long balance = 100;
         long refill = 1100;
-        long bonus = refill / 100;
-        if (refill <= 100) {
-            bonus = 0;
+        long bonus = 0;
+        if (refill > 100) {
+            bonus = refill / 100;
+            balance = balance + refill + bonus;
         }
         else {
             balance = balance + refill;
         }
-        if (refill > 100) {
-            bonus = refill / 100;
-        }
-        else {
-            balance = balance + refill + bonus;
-    }
         System.out.println(bonus);
         System.out.println(balance);
-        }
+    }
 }
